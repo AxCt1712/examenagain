e406ff81614cf2b554cea31d17c5a8f1fc075a98
AxCt1712 <Axell.tapiacastillo@gmail.com>
2025-05-20 19:55:46 -0600

    Add files via upload

---
+++ Examen/main.py
@@ -0,0 +1,13 @@
+    ### main.py ###
+from menu import mostrar_menu, ejecutar_opcion
+
+# Función principal del programa
+def main():
+    opcion = ''
+    while opcion != '3':
+        mostrar_menu()  # Mostrar menú de opciones
+        opcion = input("Seleccione una opción: ")
+        ejecutar_opcion(opcion)  # Ejecutar la opción seleccionada
+
+if __name__ == '__main__':
+    main()  # Punto de entrada del programa
